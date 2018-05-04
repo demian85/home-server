5b94cf2ec5549e42e3a2c14cae40cfcacc4f0d0d
Demian Rodriguez <demian85@gmail.com>
2018-05-03 22:00:03 -0300

    Improvements

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 require('dotenv/config');
 
 const express = require('express');
-const client = require('./lib/client');
+const api = require('./lib/api');
 
 const app = express();
 
-app.use((req, res, next) => {
-  if (req.query.key !== process.env.AUTH_KEY) {
-    res.status(401).end('Invalid auth key!');
-    return;
-  }
-  next();
-});
-
-app.get('/event/on', (req, res) => {
-  const { device } = req.query;
-
-  console.log('/event/on device:', device);
-  client.publish(`cmnd/${device}/POWER`, '1');
-
-  res.end();
-});
-
-app.get('/event/off', (req, res) => {
-  const { device } = req.query;
-
-  console.log('/event/off device:', device);
-  client.publish(`cmnd/${device}/POWER`, '0');
-
-  res.end();
-});
-
+app.use(api);
 app.listen(process.env.PORT);
