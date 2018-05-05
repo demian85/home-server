69fa23063d8a57d4fde2ba57caf9046268f3fea9
Demian Rodriguez <demian85@gmail.com>
2018-05-05 19:26:21 -0300

    Setup logger

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv/config');
 
+const logger = require('winston');
 const express = require('express');
+
+logger.level = process.env.LOG_LEVEL || 'info';
+
 const api = require('./lib/api');
 
 const app = express();
-
 app.use(api);
 app.listen(process.env.PORT);
