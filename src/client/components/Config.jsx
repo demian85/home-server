2e2d0a513ca428fb5dfebedb735aff6e75b2f892
Demian Rodriguez <demian85@gmail.com>
2018-05-22 17:37:16 -0300

    Webpack basic setup

---
+++ src/client/components/Config.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Config.css';
+
+export default function Config() {
+  return (
+    <section className={styles.root}>
+
+    </section>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/components/App.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist/client/public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
