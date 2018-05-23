ca8cbd877a9587ffcfe5337c580025d638b95743
Demian Rodriguez <demian85@gmail.com>
2018-05-22 21:27:12 -0300

    Use modules

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -5,7 +5,7 @@ import styles from './Home.css';
 export default function Home() {
   return (
     <section className={styles.root}>
-
+      Hello Home!
     </section>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,17 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
       }
     ]
   }
