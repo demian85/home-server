96b279b0a78e1050499aaafda0cf7612cc5a6f7c
Demian Rodriguez <demian85@gmail.com>
2018-05-26 18:49:08 -0300

    Fix default config

--- src/server/config.js
+++ src/server/config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  heater: {
-    defaultTriggerTemp: 23,
-    minStateDurationSecs: 60 * 15,
-    autoMode: false,
-    tempGroups: [
-      { start: 0, end: 4, temp: 23.1 },
-      { start: 4, end: 9, temp: 23.3 },
-      { start: 9, end: 18, temp: 22.5 },
-      { start: 18, end: 24, temp: 22.8 },
-    ]
-  }
+  defaultTriggerTemp: 23,
+  minStateDurationSecs: 60 * 15,
+  autoMode: false,
+  tempGroups: [
+    { start: 0, end: 4, temp: 23.1 },
+    { start: 4, end: 9, temp: 23.3 },
+    { start: 9, end: 18, temp: 22.5 },
+    { start: 18, end: 24, temp: 22.8 },
+  ]
 };
