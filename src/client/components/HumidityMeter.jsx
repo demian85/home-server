5b5ca1212ab8466fe76f4118526b8c7d633f32d1
Demian Rodriguez <demian85@gmail.com>
2018-05-28 09:17:22 -0300

    Fix layout

--- src/client/components/HumidityMeter.jsx
+++ src/client/components/HumidityMeter.jsx
@@ -8,7 +8,7 @@ export default function HumidityMeter(props) {
       <h3>{ props.title }</h3>
       <span>{ props.value } %</span>
       <div className={styles.range} style={{ backgroundImage: 'linear-gradient(to bottom, #ff0000 0%, #00ff00 50%, #0000ff 100%)' }}>
-        <div className={styles.cover} style={{ height: `${props.value}%`}} />
+        <div className={styles.cover} style={{ height: `${100 - props.value}%`}} />
       </div>
     </div>
   );
