64af2793f2d09affe1eb34a57225c1378df1324a
Demian Rodriguez <demian85@gmail.com>
2018-07-02 14:54:55 -0300

    Update config

--- src/server/config.js
+++ src/server/config.js
@@ -3,9 +3,9 @@ module.exports = {
   minStateDurationSecs: 60 * 15,
   autoMode: false,
   tempGroups: [
-    { start: 0, end: 4, temp: 23.1 },
-    { start: 4, end: 9, temp: 23.3 },
-    { start: 9, end: 18, temp: 22.5 },
-    { start: 18, end: 24, temp: 22.8 },
+    { start: 0, end: 4, temp: 24.1 },
+    { start: 4, end: 9, temp: 24.3 },
+    { start: 9, end: 18, temp: 23 },
+    { start: 18, end: 24, temp: 24 },
   ]
 };
