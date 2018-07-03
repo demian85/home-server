c81b3746a869037c8187298260e02a28df95f3f3
Demian Rodriguez <demian85@gmail.com>
2018-07-03 15:19:41 -0300

    Basic impl

--- src/client/components/TemperatureMeter.jsx
+++ src/client/components/TemperatureMeter.jsx
@@ -1,18 +1,86 @@
 import React from 'react';
+import * as Highcharts from 'highcharts';
+
+require('highcharts/highcharts-more')(Highcharts);
+require('highcharts/modules/solid-gauge')(Highcharts);
 
 import styles from './TemperatureMeter.css';
 
-export default function TemperatureMeter(props) {
-  const perc = 100 - Math.round((props.value - 10) / props.value * 100);
-  return (
-    <div className={styles.root}>
-      <div className={styles.header}>
-        <h3>{ props.title }</h3>
-        <span>{ props.value } ˚C</span>
+const gaugeOptions = {
+  title: null,
+  pane: {
+    startAngle: -90,
+    endAngle: 90,
+    background: {
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+      innerRadius: '60%',
+      outerRadius: '100%',
+      shape: 'arc'
+    }
+  },
+  tooltip: {
+    enabled: false
+  },
+  yAxis: {
+    stops: [
+      [0.1, '#55BF3B'], // green
+      [0.5, '#DDDF0D'], // yellow
+      [0.9, '#DF5353'] // red
+    ],
+    lineWidth: 0,
+    minorTickInterval: null,
+    tickAmount: 2,
+    title: {
+      y: -70
+    },
+    labels: {
+      y: 16
+    }
+  },
+  plotOptions: {
+    solidgauge: {
+      dataLabels: {
+        y: 5,
+        borderWidth: 0
+      }
+    }
+  }
+};
+
+export default class TemperatureMeter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.chart = React.createRef();
+  }
+
+  componentDidMount() {
+    Highcharts.chart(Highcharts.merge(gaugeOptions, {
+      chart: {
+        renderTo: this.chart.current,
+        type: 'solidgauge'
+      },
+      yAxis: {
+        min: 0,
+        max: 40,
+        title: {
+          text: this.props.title
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        data: [this.props.value],
+      }]
+    }));
+  }
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className={styles.chart} ref={this.chart} />
       </div>
-      <div className={styles.range} style={{ backgroundImage: 'linear-gradient(to bottom, #ff0000 0%, #00ff00 50%, #0000ff 100%)' }}>
-        <div className={styles.cover} style={{ height: `${perc}%`}} />
-      </div>
-    </div>
-  );
+    );
+  }
+
 }
