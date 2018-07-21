b3831689c0a1ad5847caf52fb7a75b5e8c96fc20
Demian Rodriguez <demian85@gmail.com>
2018-07-21 17:59:35 -0300

    Revert "Avoid duplicate weather requests"
    This reverts commit 1c7f3c04b972eee5b1f234c9884a5b63c41f1964.

--- src/server/lib/weather.js
+++ src/server/lib/weather.js
@@ -4,14 +4,7 @@ const db = require('./db');
 
 const expireTimeoutSecs = 60 * 15; // 15 minutes
 
-let weatherPromise;
-
 async function getWeather() {
-
-  if (weatherPromise) {
-    logger.debug('Returning promise...');
-    return weatherPromise;
-  }
 
   const value = await db.get('weather');
 
@@ -20,14 +13,13 @@ async function getWeather() {
     return JSON.parse(value);
   }
 
-  weatherPromise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?id=3433955&appid=${process.env.OPENWEATHER_APP_ID}&units=metric`;
 
     logger.info('requesting weather...', url);
 
     request.get({ url, json: true}, async (err, res, body) => {
       if (err) {
-        weatherPromise = null;
         return reject(err);
       }
 
@@ -37,12 +29,9 @@ async function getWeather() {
 
       logger.debug('saved updated weather report');
 
-      weatherPromise = null;
       resolve(weather);
     });
   });
-
-  return weatherPromise;
 }
 
 exports.getWeather = getWeather;
