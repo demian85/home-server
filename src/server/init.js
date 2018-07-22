8997e1feacffebbdd66743975c79e3df9ba9f792
Demian Rodriguez <demian85@gmail.com>
2018-07-22 17:04:47 -0300

    Bump version

--- src/server/init.js
+++ src/server/init.js
@@ -5,6 +5,7 @@ const defaultConfig = require('./config');
 
 (async function main() {
   await db.set('heater.config', JSON.stringify(defaultConfig));
+  console.log('Default config set:', defaultConfig);
   await db.end();
 })();
 
