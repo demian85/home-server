a25fa0efe03bfeb87241c58451e46f4b4c7b49b7
Demian Rodriguez <demian85@gmail.com>
2019-02-10 15:08:38 -0300

    Take days into account

--- src/client/components/TimeAgo.jsx
+++ src/client/components/TimeAgo.jsx
@@ -25,6 +25,7 @@ export default class TimeAgo extends React.PureComponent {
 
   render() {
     const { timeDiffSecs } = this.state;
+    const daySeconds = 3600 * 24;
 
     let friendlyTimeAgo = '';
     if (timeDiffSecs) {
@@ -32,8 +33,10 @@ export default class TimeAgo extends React.PureComponent {
         friendlyTimeAgo = `${timeDiffSecs}s ago`;
       } else if (timeDiffSecs < 3600) {
         friendlyTimeAgo = `${Math.round(timeDiffSecs / 60)}m ago`;
-      } else {
+      } else if (timeDiffSecs < daySeconds) {
         friendlyTimeAgo = `${Math.round(timeDiffSecs / 3600)}h ago`;
+      } else {
+        friendlyTimeAgo = `${Math.round(timeDiffSecs / daySeconds)}d ago`;
       }
     }
 
