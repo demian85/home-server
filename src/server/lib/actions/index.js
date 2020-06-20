867292d5551dc422c509dab72b0eba6ab99ea14c
Demian Rodriguez <demian85@gmail.com>
2020-06-20 18:42:02 -0300

    Fix undefined func

--- src/server/lib/actions/index.js
+++ src/server/lib/actions/index.js
@@ -2,20 +2,9 @@ const db = require('../db');
 const logger = require('../logger');
 const mqttClient = require('../mqtt/client');
 const topics = require('../mqtt/topics');
-const {
-  isNightTime,
-  isBedTime,
-  getMotionSensorState,
-  getRoomSetPoint,
-  getOutsideTemperature,
-} = require('../utils');
-const { turnOnDevice } = require('../main');
+const { isNightTime, isBedTime, getMotionSensorState } = require('../utils');
 
-const actions = [
-  turnOnDeskLampIfNeeded,
-  turnOffDeskLampIfNeeded,
-  // toggleBathroomHeaterIfNeeded,
-];
+const actions = [turnOnDeskLampIfNeeded, turnOffDeskLampIfNeeded];
 
 exports.runScheduledActions = function runScheduledActions() {
   logger.debug(`runScheduledActions()`);
@@ -78,25 +67,4 @@ async function turnOnDeskLampIfNeeded() {
   }
 }
 
-// turn on/off bathroom heater rail automatically
-async function toggleBathroomHeaterIfNeeded() {
-  const outsideTemperature = await getOutsideTemperature();
-  const humidity = await db.getSensorData('heaterPanel');
-  const setPoint = await getRoomSetPoint('smallRoom');
-  const currentHour = new Date().getHours();
-
-  const shouldTurnOn =
-    outsideTemperature &&
-    (humidity >= 75 || outsideTemperature < setPoint - 2) &&
-    (currentHour >= 18 || currentHour < 7);
-
-  logger.debug(`toggleBathroomHeaterIfNeeded()`, {
-    outsideTemperature,
-    humidity,
-    setPoint,
-    currentHour,
-    shouldTurnOn,
-  });
-
-  turnOnDevice('CHANGE ME', shouldTurnOn);
-}
+exports.turnOnDeskLampIfNeeded = turnOnDeskLampIfNeeded;
