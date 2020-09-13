debe23942a913710b0cb0579e83f2e471c4aa90d
Demian Rodriguez <demian85@gmail.com>
2020-09-13 13:47:05 -0300

    Disable url resolution

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = (env = {}) => {
               loader: 'css-loader',
               options: {
                 modules: true,
+                url: false,
               },
             },
           ],
