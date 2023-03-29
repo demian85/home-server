a58d94e64b14305b9c1937f677789ebb9e3d969a
Demián Rodriguez <demian85@gmail.com>
2023-03-29 13:37:35 -0300

    Fix condition

--- src/server/lib/parsers/sonoff-pool-pump.ts
+++ src/server/lib/parsers/sonoff-pool-pump.ts
@@ -6,13 +6,18 @@ let voltageIsLow: boolean | null = null
 export const sensorParser: Parser = (payload) => {
   const data = payload as TasmotaSensorPayload
   const voltage = data?.ENERGY?.Voltage ?? 0
+
   if (voltage === 0) {
     return
   }
-  if (voltage <= 205 && !voltageIsLow) {
+
+  if (voltage <= 205 && (voltageIsLow === false || voltageIsLow === null)) {
     callWebhook('device_event', 'Energy watcher', `Voltage is LOW: ${voltage}`)
     voltageIsLow = true
-  } else if (voltage >= 212 && voltageIsLow) {
+  } else if (
+    voltage >= 212 &&
+    (voltageIsLow === true || voltageIsLow === null)
+  ) {
     callWebhook(
       'device_event',
       'Energy watcher',
