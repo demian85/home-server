378f0e34c955f93bae522bdecafcf9eee75f5093
Demián Rodriguez <demian85@gmail.com>
2023-05-30 17:18:16 -0300

    Notify power on/off

--- src/server/lib/parsers/devices/sonoff-water-pump.ts
+++ src/server/lib/parsers/devices/sonoff-water-pump.ts
@@ -6,6 +6,10 @@ const parsers: Record<string, Parser> = {
     const online = String(payload).toLowerCase() === 'online'
     sendNotification(`*Water Pump* is ${online ? 'online' : 'offline'}`)
   },
+  'stat/sonoff-water-pump/POWER': (payload) => {
+    const data = payload as string
+    sendNotification(`*Water Pump* reported: Power ${data}`)
+  },
 }
 
 export default parsers
