e02b4f9f697ed5d00e82833a7c7ffd023af1187b
Demián Rodriguez <demian85@gmail.com>
2023-09-19 09:44:26 -0300

    Send notifications when no water flows

--- src/server/lib/parsers/devices/sonoff-water-pump.ts
+++ src/server/lib/parsers/devices/sonoff-water-pump.ts
@@ -1,11 +1,53 @@
-import { Parser } from '@lib/types'
+import { Parser, TasmotaSensorPayload } from '@lib/types'
 import { lwtParser, powerParser } from '../common'
+import { sendNotification } from '@lib/telegram'
+import { getDevicePower } from '@lib/db'
+import { DateTime } from 'luxon'
 
 const deviceId = 'sonoff-water-pump'
 
+const notifications: string[] = []
+
+const powerHandler = powerParser(deviceId, '🚰 Water Pump')
+
 const parsers: Record<string, Parser> = {
   'tele/sonoff-water-pump/LWT': lwtParser(deviceId, '🚰 Water Pump'),
-  'stat/sonoff-water-pump/POWER': powerParser(deviceId, '🚰 Water Pump'),
+  'stat/sonoff-water-pump/POWER': async (payload: unknown) => {
+    await powerHandler(payload)
+    notifications.splice(0, notifications.length)
+  },
+  'tele/sonoff-water-pump/SENSOR': async (payload: unknown) => {
+    const data = payload as TasmotaSensorPayload
+    const power = data?.ENERGY?.Power ?? 0
+    const powerStatus = await getDevicePower(deviceId)
+
+    if (!powerStatus || power < 100 || powerStatus.value === 'off') {
+      return
+    }
+
+    const onSince = DateTime.fromMillis(powerStatus.timestamp)
+
+    if (Math.abs(onSince.diffNow().as('seconds')) < 5) {
+      return
+    }
+
+    if (power < 300) {
+      sendNotificationOnce(`❌ No water flow!`)
+      return
+    }
+
+    if (power < 450) {
+      sendNotificationOnce(`🚰 Poor water flow!`)
+      return
+    }
+  },
+}
+
+async function sendNotificationOnce(msg: string) {
+  if (!notifications.includes(msg)) {
+    await sendNotification(msg, 'HTML')
+    notifications.push(msg)
+  }
 }
 
 export default parsers
