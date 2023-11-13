1e11146e42ad34043fd69132d85ece7ed1937399
Demián Rodriguez <demian85@gmail.com>
2023-11-13 14:16:40 -0300

    Ignore previous status

--- src/server/lib/parsers/devices/shelly-powergrid-input.ts
+++ src/server/lib/parsers/devices/shelly-powergrid-input.ts
@@ -1,12 +1,19 @@
 import { Parser, TasmotaSensorPayload } from '@lib/types'
-import { lwtParser } from '../common'
-import { setSystemStatus } from '@lib/db'
+import { setDeviceStatus, setSystemStatus } from '@lib/db'
+import { sendNotification } from '@lib/telegram'
 
 const parsers: Record<string, Parser> = {
-  'tele/shelly-powergrid-input/LWT': lwtParser(
-    'shelly-powergrid-input',
-    '⚡ PowerGrid Input'
-  ),
+  'tele/shelly-powergrid-input/LWT': async (payload: unknown) => {
+    const deviceId = 'shelly-powergrid-input'
+    const newStatus = String(payload).toLowerCase()
+    await setDeviceStatus(deviceId, newStatus)
+    await sendNotification(
+      `<b>⚡ PowerGrid Input</b> is ${
+        newStatus === 'online' ? 'online 🟢' : 'offline 🔴'
+      }`,
+      'HTML'
+    )
+  },
   'tele/shelly-powergrid-input/SENSOR': async (payload: unknown) => {
     const data = payload as TasmotaSensorPayload
     const powerGridVoltage = data?.ENERGY?.Voltage ?? 0
