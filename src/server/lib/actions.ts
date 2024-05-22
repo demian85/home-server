0324cb099c1904b5516543f9a86662f4c4a960d5
Demián Rodriguez <demian85@gmail.com>
2024-05-22 16:22:11 -0300

    Remove unused code

--- src/server/lib/actions.ts
+++ src/server/lib/actions.ts
@@ -4,41 +4,6 @@ import { sendNotification } from './telegram'
 import { client as mqttClient } from '@lib/mqtt'
 import { DateTime } from 'luxon'
 
-let tempIsHigh = false
-let humidityIsHigh = false
-
-export function highTemperatureHandler(
-  _deviceName: string,
-  temp: number | null
-) {
-  if (temp === null) {
-    return
-  }
-
-  if (temp >= 30 && !tempIsHigh) {
-    sendNotification(`🌡️ Temperature is HIGH (${temp} C)`)
-    tempIsHigh = true
-  } else if (temp < 30) {
-    tempIsHigh = false
-  }
-}
-
-export function highHumidityHandler(
-  _deviceName: string,
-  humidity: number | null
-) {
-  if (humidity === null) {
-    return
-  }
-
-  if (humidity >= 90 && !humidityIsHigh) {
-    sendNotification(`🌫️ Humidity is HIGH (${humidity}%)`)
-    humidityIsHigh = true
-  } else if (humidity < 90) {
-    humidityIsHigh = false
-  }
-}
-
 export async function automaticTemperatureHandler() {
   const { autoTemp, targetTemp: currentTargetTemp } = await getSystemStatus()
 
