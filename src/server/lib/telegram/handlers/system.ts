9861ff7e04ec74c98a49d3c9f7d2eced38ae1382
Demián Rodriguez <demian85@gmail.com>
2024-05-23 11:50:00 -0300

    Better formatting

--- src/server/lib/telegram/handlers/system.ts
+++ src/server/lib/telegram/handlers/system.ts
@@ -1,5 +1,7 @@
+import config from 'src/config'
 import { MessageContext } from '../types'
-import { getSystemStatus } from '@lib/db'
+import { getDeviceKey, getSystemStatus } from '@lib/db'
+import { DateTime } from 'luxon'
 
 export default {
   message: [
@@ -7,10 +9,56 @@ export default {
     async (ctx: MessageContext) => {
       ctx.session.currentCommand = { id: 'system', step: 0 }
       const system = await getSystemStatus()
-      await ctx.reply(`System config: <code>${JSON.stringify(system)}</code>`, {
+      const heatingDevices = config.devices.filter((v) => !!v.setTemperature)
+      const outputEntries = Object.entries(system).map(([k, v]) => {
+        return `<code>${k}: ${v}</code>`
+      })
+
+      for (const d of heatingDevices) {
+        outputEntries.push(...(await getSensorDataEntries(d.id, d.name)))
+      }
+
+      outputEntries.push(
+        ...(await getSensorDataEntries('wemos-garage', 'Outdoor sensors'))
+      )
+
+      await ctx.reply(`<b>System</b>\n${outputEntries.join('\n')}`, {
         parse_mode: 'HTML',
       })
     },
   ],
   callbackQuery: [],
+}
+
+async function getSensorDataEntries(
+  deviceId: string,
+  deviceName: string
+): Promise<string[]> {
+  const outputEntries = []
+  const temp = (await getDeviceKey(deviceId, 'temperature')) ?? null
+  const hum = (await getDeviceKey(deviceId, 'humidity')) ?? null
+  outputEntries.push(`<b>${deviceName}</b>`)
+  if (temp && temp.value !== null) {
+    outputEntries.push(
+      `<code>- Temperature: ${temp.value} C${
+        DateTime.fromMillis(temp.timestamp).diffNow('minutes').as('minutes') <
+        -5
+          ? '(outdated)'
+          : ''
+      }</code>`
+    )
+  }
+  if (hum && hum.value !== null) {
+    outputEntries.push(
+      `<code>- Humidity: ${hum.value}%${
+        DateTime.fromMillis(hum.timestamp).diffNow('minutes').as('minutes') < -5
+          ? '(outdated)'
+          : ''
+      }</code>`
+    )
+  }
+  if (outputEntries.length === 1) {
+    outputEntries.push(`<code>(no data)</code>`)
+  }
+  return outputEntries
 }
